c57aa3501b950541c19ed35bb015cdf7a8ea1152
Jiang ci <69751470+3127595118@users.noreply.github.com>
2023-01-03 11:46:56 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Zasky Blog',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1962714-ec8d3d557e4f8dc3a3d62795',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://www.jcblog.cf/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/3127595118/3127595118.github.io'
+    },
+    {
+      text: 'Blog',
+      url: 'https://www.jcblog.cf/'
+    },
+  ],
+};
